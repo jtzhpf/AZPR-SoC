feb231e3d5219cdbef863040296d533a62f82639
jtzhpf <jtzhpf@126.com>
2022-05-05 16:21:05 +0800

    nettype.h

---
+++ nettype.h
@@ -0,0 +1,7 @@
+//定义默认网络类型
+`ifndef __NETTYPE_HEADER__
+    `define __NETTYPE_HEADER__
+        /*默认网络类型有none型和wire型，推荐none型避免人为失误*/
+        `default_nettype none
+        //`default_nettype wire
+`endif
